4e518e08f6567f0d868ff618a29ad07e0b051d33
Tom Hudson <mail@tomnomnom.com>
2016-03-28 23:51:49 +0100

    Improves error handling in cmd, updates README

--- cmd/xtermcolor/main.go
+++ cmd/xtermcolor/main.go
@@ -35,17 +35,21 @@ func handleError(code int, err error) {
 }
 
 func getRGBArgs() (color.Color, error) {
+	parseErr := func(input string) error {
+		return fmt.Errorf("Failed to parse argument [%s], must be in the range 0-255", input)
+	}
+
 	r, err := strconv.ParseUint(flag.Arg(0), 10, 8)
 	if err != nil {
-		return nil, err
+		return nil, parseErr(flag.Arg(0))
 	}
 	g, err := strconv.ParseUint(flag.Arg(1), 10, 8)
 	if err != nil {
-		return nil, err
+		return nil, parseErr(flag.Arg(1))
 	}
 	b, err := strconv.ParseUint(flag.Arg(2), 10, 8)
 	if err != nil {
-		return nil, err
+		return nil, parseErr(flag.Arg(2))
 	}
 
 	return color.RGBA{uint8(r), uint8(g), uint8(b), 255}, nil
