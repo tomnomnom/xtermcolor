0e482dfa9c0bbc922491dbfa4707fb3cf24beaef
Tom Hudson <mail@tomnomnom.com>
2016-03-28 01:31:45 +0100

    Switches to supporting color.Color

--- colors_test.go
+++ colors_test.go
@@ -1,43 +1,53 @@
 package xtermcolor
 
-import "testing"
+import (
+	"image/color"
+	"testing"
+)
 
-type intTest struct {
-	in  int
-	exp int
+// RGBA Tests
+type rgbaTest struct {
+	in  color.RGBA
+	exp uint8
 }
 
-type rgbTest struct {
-	r, g, b int
-	exp     int
+var rgbaTests = []rgbaTest{
+	{color.RGBA{255, 0, 255, 255}, 13},
+	{color.RGBA{120, 120, 120, 255}, 8},
+	{color.RGBA{63, 254, 128, 255}, 84},
+	{color.RGBA{215, 0, 95, 255}, 161},
+	{color.RGBA{204, 102, 255, 255}, 177},
+	{color.RGBA{17, 156, 17, 255}, 71},
+}
+
+func TestRGBA(t *testing.T) {
+	for _, test := range rgbaTests {
+		act := FromColor(test.in)
+		if act != test.exp {
+			t.Errorf("FromRGB(%v) should be %d (%v); got %d (%v)", test.in, test.exp, colors[test.exp], act, colors[act])
+		}
+	}
+}
+
+// Int Tests
+type intTest struct {
+	in  uint32
+	exp uint8
 }
 
 var intTests = []intTest{
-	{0x000000, 0},
-	{0xFF0000, 9},
-	{0xCC66FF, 171},
-}
-
-var rgbTests = []rgbTest{
-	{255, 0, 255, 13},
-	{123, 123, 123, 8},
-	{63, 254, 128, 84},
+	{0x000000FF, 0},
+	{0xFF0000FF, 9},
+	{0xCC66FFFF, 177},
 }
 
 func TestInts(t *testing.T) {
 	for _, test := range intTests {
 		act := FromInt(test.in)
 		if act != test.exp {
-			t.Errorf("FromInt(0x%06.X) should be %d; got %d", test.in, test.exp, act)
-		}
-	}
-}
 
-func TestRGB(t *testing.T) {
-	for _, test := range rgbTests {
-		act := FromRGB(test.r, test.g, test.b)
-		if act != test.exp {
-			t.Errorf("FromRGB(%d, %d, %d) should be %d; got %d", test.r, test.g, test.b, test.exp, act)
+			//t.Errorf("FromInt(0x%06.X) should be %d; got %d", test.in, test.exp, act)
+			t.Errorf("FromInt(0x%08.X) (%v) should be %d (%v); got %d (%v)", test.in, intToRGBA(test.in), test.exp, colors[test.exp], act, colors[act])
 		}
 	}
 }
--- examples_test.go
+++ examples_test.go
@@ -1,4 +1,4 @@
-package main
+package xtermcolor_test
 
 import (
 	"fmt"
@@ -7,7 +7,16 @@ import (
 	"github.com/tomnomnom/xtermcolor"
 )
 
-func main() {
+func ExampleFromColor() {
 	fmt.Println(xtermcolor.FromColor(color.RGBA{120, 210, 120, 255}))
+
+	// Output:
+	// 114
+}
+
+func ExampleFromInt() {
 	fmt.Println(xtermcolor.FromInt(0xCC66FFFF))
+
+	// Output:
+	// 177
 }
